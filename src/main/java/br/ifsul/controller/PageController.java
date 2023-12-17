c105b89b8e086be9356a9f67e8dae6666396c98d
Patrick Timm <86640279+pctimm@users.noreply.github.com>
2023-12-17 16:42:09 -0300

    Criada tela de deputado

--- src/main/java/br/ifsul/controller/PageController.java
+++ src/main/java/br/ifsul/controller/PageController.java
@@ -1,25 +1,41 @@
 package br.ifsul.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import br.ifsul.entity.Deputado;
+import br.ifsul.entity.valueobjects.ListaDeputado;
 import br.ifsul.repository.DeputadoRepository;
+import br.ifsul.service.DeputadoService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.websocket.server.PathParam;
 
 @Controller
 public class PageController {
     @Autowired
-    private DeputadoRepository deputadoRepo;
+    private DeputadoService deputadoService;
     
     @GetMapping("/inicio")
     public String inicio(HttpServletRequest request) {
-        List<br.ifsul.entity.Deputado> listaDeputados = deputadoRepo.findAll();
-        request.setAttribute("deputados", listaDeputados);
+        ListaDeputado listaDeputados = deputadoService.getDeputadosDados();
+        request.setAttribute("deputados", listaDeputados.getDados());
         // dados.addObject("deputados", listaDeputados);
         return "inicio";
     }
+
+    @GetMapping("/deputado/{id}")
+    public String deputado(@PathVariable Long id, HttpServletRequest request) {
+        Optional<Deputado> deputado = deputadoService.getDeputado(id); 
+
+        request.setAttribute("deputados", deputado);
+        // dados.addObject("deputados", listaDeputados);
+        return "deputado";
+    }
+
+
 }
