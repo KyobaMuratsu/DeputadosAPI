407e749719bf46c19bf79d27d4e6565f6882473d
Patrick Timm <86640279+pctimm@users.noreply.github.com>
2023-12-17 18:19:18 -0300

    Criada tela cadastrar deputado em evento

--- src/main/java/br/ifsul/controller/PageController.java
+++ src/main/java/br/ifsul/controller/PageController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import br.ifsul.entity.Deputado;
+import br.ifsul.entity.Evento;
 import br.ifsul.entity.valueobjects.ListaDeputado;
+import br.ifsul.entity.valueobjects.ListaEvento;
 import br.ifsul.repository.DeputadoRepository;
 import br.ifsul.service.DeputadoService;
+import br.ifsul.service.EventoService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.websocket.server.PathParam;
 
@@ -19,6 +22,9 @@ import jakarta.websocket.server.PathParam;
 public class PageController {
     @Autowired
     private DeputadoService deputadoService;
+
+    @Autowired
+    private EventoService eventoService;
     
     @GetMapping("/inicio")
     public String inicio(HttpServletRequest request) {
@@ -43,5 +49,21 @@ public class PageController {
         return "deputado";
     }
 
+    @GetMapping("/deputado/{id}/eventos/cadastrar")
+    public String cadastraremevento(@PathVariable Long id, HttpServletRequest request) {
+        Optional<Deputado> deputadoOptional = deputadoService.getDeputado(id);
+        ListaEvento eventos = eventoService.getEventoDados();
+        
+        if (deputadoOptional.isEmpty()) {
+            return "redirect:/inicio";
+        }
+
+        Deputado deputado = deputadoOptional.get(); 
+
+        request.setAttribute("deputado", deputado);
+        request.setAttribute("eventos", eventos.getDados());
+        // dados.addObject("deputados", listaDeputados);
+        return "cadastraremevento";
+    }
 
 }
